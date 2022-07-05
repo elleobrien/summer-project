3a7c3b55ef559bbb31d5e49a13bab5d2e3b39883
Elle O'Brien <elleobrien11@gmail.com>
2022-07-05 10:52:46 -0400

    this is my first commit

---
+++ summer-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Working on a project to demo some data science skills during the Summer '22 Capstone',
+    author='Elle',
+    license='',
+)
